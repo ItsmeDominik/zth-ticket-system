80d47c007fa125e0c35b02dabeb2addbdb1853b8
ItsmeDominik <64275958+ItsmeDominik@users.noreply.github.com>
2021-12-31 16:10:59 +0100

    Create repositroy part 2

---
+++ src/main/java/com/example/zthticketsystem/ZthTicketSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.zthticketsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZthTicketSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZthTicketSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zthticketsystem/ZthTicketSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zthticketsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZthTicketSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
